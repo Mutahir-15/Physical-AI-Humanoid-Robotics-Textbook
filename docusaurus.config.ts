3a04af2dcc1e84b4d97901dab29d797c9acc0b2e
Mutahir Bin Athar <164089040+Mutahir-15@users.noreply.github.com>
2025-12-26 23:26:09 +0500

    fix_backend_build_issues

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -33,7 +33,12 @@ const config: Config = {
   projectName: 'Physical-AI-Humanoid-Robotics-Textbook', // TODO: Update with your GitHub repo name
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  markdown: {
+    format: 'detect',
+    hooks: {
+      onBrokenMarkdownLinks: 'warn',
+    },
+  },
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
